3f2ecc49b4aaa9f0eb2a1e4f2dd3d0a8291b1cdc
Radhen Khant <67072636+Radhen-123@users.noreply.github.com>
2020-06-29 07:53:17 -0400

    Update Power.py

--- Power.py
+++ Power.py
@@ -1,11 +1,12 @@
+x = 0
 def FastPower(Base, Power):                    #a^b
-    global x
+    global x                                   #to count Steps
     x += 1
-    if (Power == 0 or Base == 1):
+    if (Power == 0 or Base == 1):             # Baase Condition
         return 1
     elif (Power%2 == 0):
-        return FastPower(Base*Base, Power/2)
+        return FastPower(Base*Base, Power/2)    #example 3^6 = (3*3)^3
     else:
-        return Base*FastPower(Base, Power-1)
+        return Base*FastPower(Base, Power-1)    #example 3^3 = 3*3^2
 print(FastPower(2, 100))
-print(x)
+print("steps:"+str(x))
