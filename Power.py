f89d575cc1eae9f69440e90a2f305d8f0a28c017
Radhen Khant <67072636+Radhen-123@users.noreply.github.com>
2020-07-10 05:34:56 -0400

    Update Power.py

--- Power.py
+++ Power.py
@@ -10,3 +10,21 @@ def FastPower(Base, Power):                    #a^b
         return Base*FastPower(Base, Power-1)    #example 3^3 = 3*3^2
 print(FastPower(2, 100))
 print("steps:"+str(x))
+
+
+
+def FastPower(Base, Power):                    #a^b
+    global x
+    x += 1
+    if (Power == 0 or Base == 1):
+        return 1
+    elif Power < 0:
+        if (Power % 2 == 0):
+            return FastPower(1 / (Base * Base), Power / 2)
+        else:
+            return FastPower(Base, Power + 1) / Base
+    elif (Power%2 == 0):
+        return FastPower(Base*Base, Power/2)
+    else:
+        return Base*FastPower(Base, Power-1)
+print(FastPower(4, -4))
