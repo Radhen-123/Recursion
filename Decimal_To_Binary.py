4eb26f562a2c531c70c1a73035d3c40f992779bd
Radhen Khant <67072636+Radhen-123@users.noreply.github.com>
2020-07-14 06:05:54 -0400

    Update Decimal_To_Binary.py
    Decimal to any Base

--- Decimal_To_Binary.py
+++ Decimal_To_Binary.py
@@ -3,3 +3,12 @@ def Decimal_To_Binary(Decimal):
         return Decimal
     else:
         return 10 * (Decimal_To_Binary(Decimal // 2)) + Decimal % 2
+    
+    
+def Decimal_To_Any_Base(Decimal, Base):
+    if Decimal < Base:
+        return Decimal
+    else:
+        return  10 * (Decimal_To_Any_Base(Decimal // Base, Base)) + Decimal % Base
+
+print(Decimal_To_Any_Base(11, 8))
