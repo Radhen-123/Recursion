3f54eb2d580885183fb854f661eb5ed6e1b9bfad
Radhen Khant <67072636+Radhen-123@users.noreply.github.com>
2020-07-15 12:23:59 -0400

    Create Palindrome.py
    Palindrome And Reverse String

---
+++ Palindrome.py
@@ -0,0 +1,14 @@
+def Reverse_String(Name):
+    if Name == "":
+        return ""
+    else:
+        return Reverse_String(Name[1:]) + Name[0]
+        
+def Palindrome(Name):
+    Name = Name.lower()
+    Length = len(Name)
+    if Length == 1:
+        return True
+    else:
+        return (Name[0] == Name[Length - 1] and Palindrome(Name[1:Length - 1]))
+# print(Palindrome("Radar"))
